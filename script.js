976025fe3832b451dafdc34be4695bd19d55a10c
Eric Johnson <johnsoneric586@gmail.com>
2024-03-23 11:00:52 -0400

    Finished the menu hover affect.

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
 'use strict';
 
 // ----------Regular Variables----------
-const nav = document.getElementById('nav');
-const navLogo = document.getElementById('nav-logo');
-const navLinks = document.querySelectorAll('.nav-link');
-const navSignupBtn = document.getElementById('nav-signup-btn');
 
 // ----------Functions----------
+function handleHover(event) {
+  const link = event.target.closest('.nav-link');
+  const siblings = Array.from(
+    link.closest('nav').querySelectorAll('.nav-link')
+  );
+  const logo = document.getElementById('nav-logo');
+
+  siblings.forEach(sibling => {
+    if (sibling !== link) {
+      sibling.style.transition = 'all 0.2s ease';
+      sibling.style.opacity = this;
+    }
+  });
+
+  if (link !== logo) {
+    logo.style.transition = 'all 0.2s ease';
+    logo.style.opacity = this;
+  }
+}
 
 // ----------Event Listeners----------
-
-// If one nav element is hovered, the others are grayed out
-
-nav.addEventListener('mouseover', function (event) {
-  navLinks.forEach(link => {
-    link.style.opacity = '0.5';
-  });
-
-  if (event.target !== navLogo) navLogo.style.opacity = '0.5';
-  if (event.target !== navSignupBtn) navSignupBtn.style.opacity = '0.5';
-
-  event.target.closest('.nav-link').style.opacity = '1';
-  //   event.target.closest(`${navSignupBtn}`).style.opacity = '1';
-  // TODO keep working on this hover affect for the nav, probably need to redo all the logic here b/c the real version is much better
-});
-
-nav.addEventListener('mouseout', function () {
-  navLinks.forEach(link => {
-    link.style.opacity = '1';
-  });
-
-  navLogo.style.opacity = '1';
-});
+// Menu hover
+nav.addEventListener('mouseover', handleHover.bind('0.5'));
+nav.addEventListener('mouseout', handleHover.bind('1'));
